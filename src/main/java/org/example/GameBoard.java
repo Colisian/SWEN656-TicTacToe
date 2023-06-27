5cff558bf3ad5f9cf6317cb1b40307ef9bf89c97
Colisian McLeod <mcleodom16@gmail.com>
2023-06-27 17:39:41 -0400

    Worked in TicTacToe class and Gameboard class. Created the game board and established the key attributes.

--- src/main/java/org/example/GameBoard.java
+++ src/main/java/org/example/GameBoard.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class GameBoard {
+    private TicTacToe game;
+    private Scanner scanner;
+
+    public GameBoard(TicTacToe game){
+        this.game = game;
+        this.scanner = new Scanner(System.in);
+    }
+    public void startGame(){
+        //Displays board as long as game has not been completed
+        while (!game.gameWin() && !game.gameTie()){
+            displayBoard();
+        }
+    }
+    private void displayBoard(){
+        game.displayBoard();
+    }
 }
--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,6 +1,38 @@
 package org.example;
 
 public class TicTacToe {
+
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToe(){
+        board = new char[3][3];
+        currentPlayer = 'X';
+        createBoard();
+    }
+
+
+    //Uses for loops to establish the game board
+    public void createBoard(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+
+            }
+        }
+    }
+
+    public void displayBoard(){
+        System.out.println("Current Board");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
