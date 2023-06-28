45c049be9d99c538837b19e42198a95888993d38
Colisian McLeod <mcleodom16@gmail.com>
2023-06-28 09:19:01 -0400

    Created rules for winning.

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -11,6 +11,34 @@ public class TicTacToe {
         createBoard();
     }
 
+    public void play(int row, int column){
+        board[row][column] = currentPlayer;
+        displayBoard();
+        //Flip player from X to O after each succesful move.
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+    public boolean gameWin(){
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-'){
+                return true;
+            }
+        }
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '-' ){
+                return true;
+            }
+        }
+        //check diagonal
+        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+            return true;
+        }
+        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+            return true;
+        }
+        return false;
+    }
+
+
 
     //Uses for loops to establish the game board
     public void createBoard(){
